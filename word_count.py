9a5d4dab9cc49383fb7efc875ec41812c2ca5a5b
mygithub93 <osama.mansoor1@yahoo.com>
2022-11-18 09:00:38 +0530

    spark hello world

---
+++ word_count.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from operator import add
+from pyspark.sql import SparkSession
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+def main():
+    spark = SparkSession.builder.getOrCreate()
+    file_path = 'C:\\Users\\osama\\PycharmProjects\\sparkTraining\\data\\word.txt'
+    lines = spark.read.text('C:\\Users\\osama\\PycharmProjects\\sparkTraining\\data\\word.txt') \
+                        .rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                    .map(lambda x: (x, 1)) \
+                    .reduceByKey(add)
+    res = counts.collect()
+    for word, count in res:
+        print(f'{word}: {count}')
+    spark.stop()
+
+if __name__ == '__main__':
+    main()
